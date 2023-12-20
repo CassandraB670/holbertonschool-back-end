d058e0ade03b654b315b4b0f39b7c46be107077c
CassandraB670 <cassandra.boudier@gmail.com>
2023-12-20 11:15:29 +0100

    change all

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""Script that export data in JSON format"""
-
+""" script to export data in the JSON format """
 import json
 import requests
-import sys
+from sys import argv
 
 API_URL = 'https://jsonplaceholder.typicode.com'
 
+
 if __name__ == '__main__':
+    USER_ID = argv[1]
 
-    USER_ID = sys.argv[1]
+    """ user information """
+    user_response = requests.get(f"{API_URL}/users/{USER_ID}").json()
 
-    user = requests.get(f'{API_URL}/users/{USER_ID}').json()
-    todo_list = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
+    """ todo list for the given user """
+    todo_response = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
 
-    json_filename = f"{USER_ID}.json"
-
+    """ data for export """
     data = {
         USER_ID: [
             {
                 "task": task['title'],
                 "completed": task['completed'],
-                "username": user['username']
+                "username": user_response['username']
             }
-            for task in todo_list
+            for task in todo_response
         ]
     }
-    with open(json_filename, 'w') as json_file:
+    """ write to json file """
+
+    with open(f"{USER_ID}.json", mode='w') as json_file:
         json.dump(data, json_file)
+
+    print(f"Data as been exported to {USER_ID}.json")
