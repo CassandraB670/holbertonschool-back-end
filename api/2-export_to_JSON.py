6f5fffde7c39655b181b1aa42031706569e61667
CassandraB670 <cassandra.boudier@gmail.com>
2023-12-20 11:07:22 +0100

    change line 31, add line 18 and delete last line

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,11 +8,14 @@ import sys
 API_URL = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
+
     USER_ID = sys.argv[1]
 
     user_request = requests.get(f'{API_URL}/users/{USER_ID}').json()
 
     todo_list = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
+
+    json_filename = f"{USER_ID}.json"
 
     data = {
         USER_ID: [
@@ -25,7 +28,5 @@ if __name__ == '__main__':
         ]
     }
 
-    with open(f"{USER_ID}.json", mode='w') as json_file:
+    with open(json_filename, 'w') as json_file:
         json.dump(data, json_file)
-
-        print(f"Data as been exported to {USER_ID}.json")
